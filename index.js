7e9b9f789c1044d5029126a2657c73748dd36157
lzt <842835321@qq.com>
2022-06-29 08:46:48 +0800

    refactor: limit 100

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require("./http");
-const pushMessage = require('./pushMessage');
+const pushMessage = require("./pushMessage");
 
 const calcEfficiencyRate = (yesCnt, noCnt) => {
-    const value = (yesCnt / (yesCnt + noCnt) * 100).toFixed(2);
-    return `${value}%`; 
-}
+  const value = ((yesCnt / (yesCnt + noCnt)) * 100).toFixed(2);
+  return `${value}%`;
+};
 
 const getGiftKey = () => {
   const data = {
@@ -18,18 +18,20 @@ const getGiftKey = () => {
     console.log(res.data);
     const { code, data, message } = res.data;
     let result = [];
-    if(code == 200) {
-        result = data.map(item => ({
-            key: item.content,
-            gift: item.reward,
-            useful: calcEfficiencyRate(item.sign_yes_count, item.sign_no_count),
-            updateTime: item.updated_at
-        }))
-        pushMessage.pushResult(result);
+    if (code == 200) {
+      result = data.reduce((f, item, index) => {
+        f += `${index + 1}. 【${item.content}】：${
+          item.reward
+        }，有效率：${calcEfficiencyRate(
+          item.sign_yes_count,
+          item.sign_no_count
+        )}，更新时间：${item.updated_at} \n`;
+        return f;
+      });
+      pushMessage.pushResult(result);
     } else {
-        pushMessage.pushResult(message);
+      pushMessage.pushResult(message);
     }
   });
-  
 };
 getGiftKey();
